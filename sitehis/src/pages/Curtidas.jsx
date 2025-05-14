c6d55da5e124c887afb6273874d0ec62fb7f3947
Murilo Correia <mucorreia008@gmail.com>
2025-05-14 08:52:41 -0300

    comit

--- sitehis/src/pages/Curtidas.jsx
+++ sitehis/src/pages/Curtidas.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+
 import Header from "../components/header";
-import '../styles/page.css';
+import "../styles/page.css";
+
 function Curtidas() {
+ 
+
   return (
-
     <div>
-                  <Header />
-
+      <Header />
       <h1>Curtidas</h1>
-
       <section className="bloco">
-        
+       
       </section>
     </div>
   );
--- sitehis/src/pages/Pagina9.jsx
+++ sitehis/src/pages/Pagina9.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Header from "../components/header";
+
 import "../styles/page.css";
 import imageG from "../assets/grupo.jpg";
+
 function Pagina9() {
   return (
     <div>
@@ -9,24 +11,25 @@ function Pagina9() {
       <h1 className="hero">Sobre o Grupo</h1>
       <img src={imageG} className="imageg" />
       <section className="bloco">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut autem vel
-        eveniet consequuntur maiores recusandae dolorem laboriosam perspiciatis
-        facere. Eveniet voluptate temporibus ad sunt ducimus in consequuntur
-        totam quo corporis?Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Cum incidunt praesentium omnis, unde similique, maiores eveniet
-        mollitia earum perferendis tempore nulla in eum sunt expedita iure
-        numquam architecto officiis itaque. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Dicta eligendi, natus, rerum molestiae,
-        quod eos maxime eum cumque libero necessitatibus sequi! Libero iure ut
-        facilis minus neque harum sit consequuntur. Lorem, ipsum dolor sit amet
-        consectetur adipisicing elit. Magni repellat quia blanditiis eos maxime
-        accusamus asperiores magnam! Praesentium asperiores aperiam quas id ab
-        incidunt perspiciatis? Blanditiis fugit minus a quis? Lorem, ipsum dolor
-        sit amet consectetur adipisicing elit. Et nulla eveniet inventore,
-        labore fugiat iure eius doloribus repellat omnis obcaecati eum mollitia.
-        Natus reiciendis suscipit voluptatum quas rerum pariatur necessitatibus.
-      </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut autem vel
+          eveniet consequuntur maiores recusandae dolorem laboriosam perspiciatis
+          facere. Eveniet voluptate temporibus ad sunt ducimus in consequuntur
+          totam quo corporis?Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Cum incidunt praesentium omnis, unde similique, maiores eveniet
+          mollitia earum perferendis tempore nulla in eum sunt expedita iure
+          numquam architecto officiis itaque. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Dicta eligendi, natus, rerum molestiae,
+          quod eos maxime eum cumque libero necessitatibus sequi! Libero iure ut
+          facilis minus neque harum sit consequuntur. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Magni repellat quia blanditiis eos maxime
+          accusamus asperiores magnam! Praesentium asperiores aperiam quas id ab
+          incidunt perspiciatis? Blanditiis fugit minus a quis? Lorem, ipsum dolor
+          sit amet consectetur adipisicing elit. Et nulla eveniet inventore,
+          labore fugiat iure eius doloribus repellat omnis obcaecati eum mollitia.
+          Natus reiciendis suscipit voluptatum quas rerum pariatur necessitatibus.
+        </p>
+      
       </section>
     </div>
   );
