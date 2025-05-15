92e673e015ca8d1ee8cf9a542dd191257176a5a2
Felipe Cagnin <cagnin.lima.2007@gmail.com>
2025-05-15 15:45:39 -0300

    Adicionado Carroussel

--- sitehis/src/pages/HomePage.jsx
+++ sitehis/src/pages/HomePage.jsx
@@ -21,6 +21,7 @@ function Home() {
         <>
             <Header titulo="História do Século XX"/>
 
+            <section className="bloco">
             {carrousel == 0 ? <img src="https://ogimg.infoglobo.com.br/in/23223989-970-9a5/FT1500A/652/41704050_1602.1979ARQUIVOINTER1-GUERRA-MUNDIAL-1.jpg" alt="carrouselFront" className="ImagemCarrocel" /> : ""}
           {carrousel == 1 ? <img src="https://investidorsardinha.r7.com/wp-content/uploads/2020/08/crise-de-1929-a-grande-depressao-economica.jpg" alt="carrouselFront" className="ImagemCarrocel" /> : ""}
           {carrousel == 2 ? <img src="https://s1.static.brasilescola.uol.com.br/be/conteudo/images/benito-mussolini-ao-lado-hitler-foi-grande-lider-fascismo-italiano-movimento-politico-surgido-em-1919-5bc6344f74913.jpg" alt="carrouselFront" className="ImagemCarrocel" /> : ""}
@@ -28,7 +29,6 @@ function Home() {
            {carrousel == 4 ? <img src="https://static.mundoeducacao.uol.com.br/mundoeducacao/2020/02/guerra-dos-canudos.jpg" alt="carrouselFront" className="ImagemCarrocel" /> : ""}
            {carrousel == 5 ? <img src="https://upload.wikimedia.org/wikipedia/commons/7/79/Armed_soldiers_carry_a_banner_reading_%27Communism%27%2C_Nikolskaya_street%2C_Moscow%2C_October_1917.jpg" alt="carrouselFront" className="ImagemCarrocel" /> : ""}
          
-            <section className="bloco">
                 <p>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores veniam laboriosam, tenetur consequatur, architecto sed error odit odio omnis distinctio optio quos numquam cumque consectetur pariatur qui saepe? Consectetur, esse?
                 </p>
