cbc1aaa8fc2d155ec61d4e1367747ab84efc28c7
MuriloBesson68 <ggwp.capudo@gmail.com>
2025-05-21 10:49:12 -0300

    video sobre nós

--- sitehis/src/pages/SobreNos.jsx
+++ sitehis/src/pages/SobreNos.jsx
@@ -34,6 +34,19 @@ function SobreNos() {
           um dia normal com a gente.
         </p>
       </section>
+      <br></br>
+      <article>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/dZx7uNhlaXU?si=h53z_dlcssstRSN7"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </article>
       <Footer />
     </>
   );
